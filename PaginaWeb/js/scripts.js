78b365a72b14e201af039f4393942c8bbc617300
Adolfo Laviana <adolfoalllaviana@gmail.com>
2021-01-16 14:16:48 +0100

    1.1 added CV

--- PaginaWeb/js/scripts.js
+++ PaginaWeb/js/scripts.js
@@ -2,7 +2,7 @@
     (function ($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
+    // movimiento del scroll
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
@@ -26,18 +26,18 @@
         }
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
+    // cierre menu responsive
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
+    // activa el scrollspy
     $("body").scrollspy({
         target: "#mainNav",
         offset: 100,
     });
 
-    // Collapse Navbar
+    // cierra navbar
     var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
@@ -45,8 +45,8 @@
             $("#mainNav").removeClass("navbar-shrink");
         }
     };
-    // Collapse now if page is not at top
+    // cierra la navbar si la pagina no esta en top
     navbarCollapse();
-    // Collapse the navbar when page is scrolled
+    //cierra navbar
     $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+})(jQuery); 
